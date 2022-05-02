6e1a9a304e490b967aeab5268784108ceb301418
ernestol1977 <ernestolondero@gmail.com>
2022-05-02 18:53:10 -0300

    incorporando navbar

---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,34 @@
+
+import { Container, Nav, Navbar } from "react-bootstrap"
+
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <div  className="bg-img">
+            <div>
+                <h1 className="title">Nativa Collection</h1>
+            </div>
+            <div>
+                <div>
+                    <Navbar expand="sm" navbar-center>
+                        <Container fluid>
+                            <Navbar.Brand href="#"></Navbar.Brand>
+                            <Navbar.Toggle aria-controls="navbarScroll" />
+                            <Navbar.Collapse id="navbarScroll">
+                                <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+                                    <Nav.Link href="#" className="navbar">Home</Nav.Link>
+                                    <Nav.Link  href="#laMarca" className="navbar">La marca</Nav.Link>
+                                    <Nav.Link href="#corpinos" className="navbar">Corpiños</Nav.Link>
+                                    <Nav.Link href="#bombachas" className="navbar">Bombachas</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
